54f6d7084a6d40a591af93145ef52c377eb899cd
Francisco Roldan <franciscoproldan@gmail.com>
2021-10-22 14:40:43 -0300

    feat: implementation for translated

--- parler/managers.py
+++ parler/managers.py
@@ -70,7 +70,6 @@ class TranslatableQuerySet(QuerySet):
             and isinstance(self._result_cache[0], models.Model)
         ):
             for obj in self._result_cache:
-                self.load_translations(obj)
                 obj.set_current_language(self._language)
 
     def _extract_model_params(self, defaults, **kwargs):
